b79fc0110dd86fc1557914a27d029af376eb2891
NofathZ <nofath86@gmail.com>
2021-12-11 06:22:30 +0700

    make login auth using passport session

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, UseGuards, Request, Post } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { AuthenticatedGuard } from "./auth/authenticated.guard";
+import { LocalAuthGueard } from "./auth/local-auth.guard";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @UseGuards(LocalAuthGueard)
+  @Post('login')
+  login(@Request() req): any {
+    return {msg: "Logged in"}
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Get('protected')
+  getHello(@Request() req): string {
+    return req.user
   }
 }
